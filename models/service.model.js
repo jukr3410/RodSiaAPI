c507037ccfa9ee7d2f2edd07bf87bd655587be45
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-10 21:56:12 +0700

    Update service.model.js

--- models/service.model.js
+++ models/service.model.js
@@ -23,7 +23,7 @@ const servicesSchema = new Schema({
         type:Schema.Types.ObjectId,
         ref:Garage
     },
-    Images: [{
+    images: [{
         type: Schema.Types.ObjectId,
         ref: 'FileUpload'
     }]
