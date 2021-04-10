24d09b2076729d8ee9a5555e1b5804c242496b2d
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-10 22:25:48 +0700

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -34,7 +34,8 @@ const userSchema = new Schema({
         brand:String,
         model:String,
         type:String,
-        year:String
+        year:String,
+        fuelType:String
     }],
 
     reviews: [{type: Schema.Types.ObjectId, ref: 'Review'}],
