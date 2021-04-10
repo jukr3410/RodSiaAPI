0a268e67d8a0b197da6d62be9112cbf540eff522
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-11 01:53:19 +0700

    Create serviceType.routes.js

---
+++ routes/serviceType.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const serviceType = require('../controllers/serviceType.controller');
+
+router.get('/',serviceType.getAllServiceType);
+router.get('/:id',serviceType.getServiceType);
+router.post('/',serviceType.addServiceType);
+router.put('/:id',serviceType.editServiceType);
+router.patch('/:id',serviceType.editServiceType);
+router.delete('/:id',serviceType.deleteServiceType);
+
+module.exports = router;
