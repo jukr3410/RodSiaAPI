8c47c55e8b2ea90e3cf0da39526dec9a2f49ea42
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-10 19:45:01 +0700

    update user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,23 +1,29 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET || 'JWT_SUPER_SECRET';
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     id:{
         type:Number,
-        required:true
+        required:[true, "can't be blank"]
     },
     name:{
         type:String,
-        required:true
+        required:[true, "can't be blank"]
     },
     email:{
         type:String,
-        lowercase: true
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
     },
     phone:{
         type:String,
-        required:true,
-        unique: true
+        required:[true, "can't be blank"],
+        unique: true,
+        match: [/^[0-9]+$/, 'is invalid'],
+        index: true
     },
     password:{
         type:String,
@@ -32,7 +38,21 @@ const userSchema = new Schema({
         model:String,
         type:String,
         year:String
-    }]
-})
+    }],
 
-module.exports = mongoose.model('User',userSchema)
+    
+
+});
+
+
+userSchema.path('password', {
+    // เข้ารหัส password 
+
+    set: function(password) {
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        return hashedPassword;
+    }
+});
+
+module.exports = mongoose.model('User',userSchema);
