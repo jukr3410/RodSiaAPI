74b4dba6c1762220addb6a9423638e88b2edb855
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-10 21:38:59 +0700

    Update fileUpload.model.js

--- models/fileUpload.model.js
+++ models/fileUpload.model.js
@@ -5,5 +5,18 @@ const fileUploadSchema = new  Schema({
     fileName: {
         type: String
     },
-    
+    filePath: {
+        type: String
+    },
+    service: {
+        type: Schema.Types.ObjectId, ref: 'Service'
+    },
+    garage: {
+        type: Schema.Types.ObjectId, ref: 'Garage'
+    },
+    infoAssistant: {
+        type: Schema.Types.ObjectId, ref: 'InfoAssistant'
+    }
 });
+
+module.exports = mongoose.model('FileUpload', fileUploadSchema);
