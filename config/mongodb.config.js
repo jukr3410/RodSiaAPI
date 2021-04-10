327c9c4ff6c6382b1e1bfa5e5ccb646bbcd68caa
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-10 16:29:17 +0700

    created config db file

---
+++ config/mongodb.config.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+expert.configure = function() {
+    mongoose.Promise = global.Promise;
+    mongoose.set('debug', true);
+    mongoose.connection.on('error', function(err) {
+        console.error(err);
+    });
+    mongoose.connection.once('open', () => {
+        console.log('[+] Connected to database seccessfully');
+    });
+
+    return mongoose.connect(process.env.MONGODB_URI, {
+        debug: process.env.DEBUG || true,
+        keepAlive: true,
+        useNewUrlParser: true,
+    });
+}
