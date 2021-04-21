94eec625ca5220d02cd35f636d112423f324e24e
juk <jaturong.3410@mail.kmutt.ac.th>
2021-04-21 15:28:00 +0700

    Update fileUpload.routes.js

--- routes/fileUpload.routes.js
+++ routes/fileUpload.routes.js
@@ -5,10 +5,13 @@ const multipart = require('connect-multiparty')
 const multipartMiddleware = multipart()
 
 router.get('/file-uploads', fileUpload.displayForm);
+router.get('/garages/:id/images', fileUpload.displayForm);
+router.get('/garinfo-assistants/:id/images', fileUpload.displayForm);
+
 // router.get('/file-uploads/:id', fileUpload.getFileUpload);
-router.post('/file-uploads', multipartMiddleware, fileUpload.upload);
-// router.put('/file-uploads/:id', fileUpload.editFileUpload);
-// router.patch('/file-uploads/:id', fileUpload.editFileUpload);
+router.post('/garages/:id/file-uploads', multipartMiddleware, fileUpload.upload);
+router.post('/info-assistants/:id/file-uploads', multipartMiddleware, fileUpload.upload);
+
 // router.delete('/file-uploads/:id', fileUpload.deleteFileUpload);
 
 module.exports = router;
