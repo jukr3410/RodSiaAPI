38d396dba6ecc76cf7e013bb7f2ce9e598f619d6
jak <56501828+jukr3410@users.noreply.github.com>
2021-09-25 14:03:58 +0700

    [update] script

---
+++ scripts/application_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo chmod -R 777 /home/ec2-user/express-app
+#navigate into working directory where we have all github files
+cd /home/ec2-user/express-app
+
+# Add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion
+
+#install node modules
+npm install
+
+
+#start our node app in the background
+node app.js > app.out.log 2> app.err.log < /dev/null &
