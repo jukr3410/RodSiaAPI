cfe240e339a23191b6ce1a6d0d238462035203d2
jak <56501828+jukr3410@users.noreply.github.com>
2021-11-18 11:37:06 +0700

    Update garage.controller.js

--- controllers/garage.controller.js
+++ controllers/garage.controller.js
@@ -293,8 +293,8 @@ module.exports.getAllGarageByQuery = async (req, res) => {
 
   const serviceType = req.query.serviceType;
   const carType = req.query.carType;
-  const lat = req.query.lat;
-  const long = req.query.long;
+  const currentLat = req.query.lat;
+  const currentLong = req.query.long;
 
   console.log(serviceType + " : " + carType);
 
@@ -339,7 +339,36 @@ module.exports.getAllGarageByQuery = async (req, res) => {
     .skip(skipIndex)
     .populate([])
     .then((garages) => {
+      if (
+        currentLat != "" &&
+        currentLat != null &&
+        currentLong != "" &&
+        currentLong != null
+      ) {
+      garages.forEach(garage => {
+        console.log("Geo: ", garage.address.geoLocation);
+        console.log("Distance: "+getDistanceFromLatLongInKm(parseFloat(currentLat),parseFloat(currentLong),parseFloat(garage.address.geoLocation.lat),parseFloat(garage.address.geoLocation.long)));
+      });}
+      
       res.status(200).json(garages);
     })
     .catch((err) => console.log(err));
 };
+
+function getDistanceFromLatLongInKm(lat1,long1,lat2,long2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLong = deg2rad(long2-long1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLong/2) * Math.sin(dLong/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var distance = R * c; // Distance in km
+  return distance;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
